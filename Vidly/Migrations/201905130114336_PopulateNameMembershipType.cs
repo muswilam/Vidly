d46f0c77c5d293a2a8bdbd6f71752f28632599ee
Sully <jackswilam@gmail.com>
2019-05-13 03:25:19 +0200

    Exercise 1 customize Domain model, update Database, Populate data into new column

---
+++ Vidly/Migrations/201905130114336_PopulateNameMembershipType.cs
@@ -0,0 +1,20 @@
+namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateNameMembershipType : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("Update MembershipTypes Set Name = 'Pay as You Go' where Id = 1 ");
+            Sql("Update MembershipTypes Set Name = 'Monthly' where Id = 2 ");
+            Sql("Update MembershipTypes Set Name = 'Quarterly' where Id = 3 ");
+            Sql("Update MembershipTypes Set Name = 'Yearly' where Id = 4 ");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}

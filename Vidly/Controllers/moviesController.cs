1187f7b106e04aa7e7aaf97897cd0682ffd28097
Sully <jackswilam@gmail.com>
2019-05-13 02:35:11 +0200

    add Eager Loading to reference another table

---
+++ Vidly/Controllers/moviesController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.Custom_ViewModel;
+using System.Data.Entity;
+
+namespace Vidly.Controllers
+{
+    public class moviesController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public moviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        public ActionResult Index()
+        {
+            var customers = _context.Customers.Include(c => c.MemberShipType).ToList();
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(cust => cust.CustomerId == id);
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        //private IEnumerable<Customer> GetCustomers()
+        //{
+        //    return new List<Customer>
+        //    {
+        //        new Customer { CustomerId = 1 , Name = "Jack David"},
+        //        new Customer { CustomerId = 2 , Name = "Mary William"}
+        //    };
+        //}
+    }
+}
---
+++ Vidly/Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        [Required]
+        [StringLength(225)]
+        public string Name { get; set; }
+        public bool IsSubscribedToNewsLetter { get; set; }
+
+        public MembershipType MemberShipType { get; set; }
+        public byte MembershipTypeId { get; set; }
+        public virtual Movie Movie { get; set; }
+    }
+}

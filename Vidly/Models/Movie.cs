435f9d179e91cf37ff39dee348a50e284b09a997
Sully <jackswilam@gmail.com>
2019-05-13 02:35:11 +0200

    add Eager Loading to reference another table

---
+++ Vidly/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int CustomerId { get; set; }
+        public virtual List<Customer> Customers { get; set; }
+    }
+}

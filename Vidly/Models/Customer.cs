53b7de757df5068ab4c3a3e4efcf8e0cd65a6695
Sully <jackswilam@gmail.com>
2019-05-15 22:57:12 +0200

    Add Validation , Styling Validation and Make a Custom Validation check the age of customer is over 18 to make membership

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -3,20 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
+using Vidly.Common;
 
 namespace Vidly.Models
 {
     public class Customer
     {
         public int CustomerId { get; set; }
+
         [Required]
         [StringLength(225)]
         public string Name { get; set; }
+
         public bool IsSubscribedToNewsLetter { get; set; }
+
         [Display(Name = "Date Of Birth")]
+        [MinYearMemberIs18]
         public Nullable<DateTime> BirthDate { get; set; }
 
         public MembershipType MemberShipType { get; set; }
+      
         [Display(Name = "Membership Type")]
         public byte MembershipTypeId { get; set; }
 
